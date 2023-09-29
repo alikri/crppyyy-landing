c861b62885fe49b015cd4864ce41dc7066a98200
Alina Nosovets <alinanosovets6849@gmail.com>
2023-09-29 12:01:57 +0200

    feat: add bots pictures

--- crypto-pay/components/Intro/Intro.jsx
+++ crypto-pay/components/Intro/Intro.jsx
@@ -1,7 +1,29 @@
 import styles from './intro.module.css';
 import { Phone } from '../Phone/Phone';
+import Image from 'next/image';
+
+const botsImages = [
+  '/bots/bot1.svg',
+  '/bots/bot2.svg',
+  '/bots/bot3.svg',
+  '/bots/bot4.svg',
+  '/bots/bot5.svg',
+];
 
 export const Intro = () => {
+  const bots = Array.from({ length: 5 }).map((_, i) => {
+    return (
+      <Image
+        key={`bot${i}`}
+        className={styles[`bot-image-${i}`]}
+        src={botsImages[i]}
+        alt="Crypto Bot"
+        width={96}
+        height={96}
+      />
+    );
+  });
+
   return (
     <section className={styles['intro']}>
       <div className={styles['intro-content']}>
@@ -13,6 +35,10 @@ export const Intro = () => {
           services.
         </h2>
         <button className={styles['intro-button']}>Get started</button>
+        <div className={styles['bots-container']}>{bots}</div>
+        <h2 className={styles['intro-subtitle']}>
+          Popular bots use Crypto Pay
+        </h2>
       </div>
       <Phone />
     </section>
