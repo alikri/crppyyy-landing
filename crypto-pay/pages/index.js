0799fafd1c1ce9643ddfb43c57d79b5546ef5523
Alina Nosovets <alinanosovets6849@gmail.com>
2023-10-02 18:31:35 +0200

    feat: add cryptocurrency range

--- crypto-pay/pages/index.js
+++ crypto-pay/pages/index.js
@@ -1,5 +1,6 @@
 import { Features } from '@/components/Features/Features';
 import { Intro } from '@/components/Intro/Intro';
+import { Payments } from '@/components/Payments/Payments';
 import Head from 'next/head';
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <main className="main-content">
         <Intro />
         <Features />
+        <Payments />
       </main>
     </>
   );
