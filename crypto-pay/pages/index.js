262c591b758a77cacfcfd8f3fb3b7dc3e6c765ed
Alina Nosovets <alinanosovets6849@gmail.com>
2023-10-09 20:15:44 +0200

    fix: delete console.log

--- crypto-pay/pages/index.js
+++ crypto-pay/pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
 
   useEffect(() => {
     const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)');
-    console.log(darkThemeMq);
 
     const handleChange = (event) => {
       setDarkTheme(event.matches);
